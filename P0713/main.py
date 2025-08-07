5d4099d057b9b039c251d5582a0eadb2967ec098
zdzbutterhead <zdz1411061965@163.com>
2025-08-07 18:42:16 +0800

    250807  18:42

--- P0713/main.py
+++ P0713/main.py
@@ -878,3 +878,33 @@ class HashMapOpenAddressing:
             else:
                 print(pair.key, "->", pair.val)
 '''
+
+
+
+def add_hash(key:str) -> int:
+    hash = 0
+    modulus = 1000000007
+    for c in key:
+        hash += ord(c)
+    return hash % modulus
+
+def mul_hash(key:str) -> int:
+    hash = 0
+    modulus = 1000000007
+    for c in key:
+        hash = 31*hash + ord(c)
+    return hash % modulus
+
+def xor_hash(key:str) -> int:
+    hash = 0
+    modulus = 1000000007
+    for c in key:
+        hash ^= ord(c)
+    return hash % modulus
+
+def rot_hash(key:str) -> int:
+    hash = 0
+    modulus = 1000000007
+    for c in key:
+        hash = (hash << 4) ^ (hash >> 28) ^ ord(c)
+    return hash % modulus
